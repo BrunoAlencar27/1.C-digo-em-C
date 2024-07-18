f36742c3d0f71fc8320c463ff386f1550379e305
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2024-07-18 18:29:31 -0300

    18/07

---
+++ 1.Básicos/3.Entrada de dados copy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int idade = 0;
+
+int main(){
+
+    printf("Valor inicial da idade: %d\n",idade);
+    printf("Por favor informe o valor da sua idade: ");
+    scanf("%d",&idade);
+    printf("Ok, sua idade é: %d\n",idade);
+    return 0;
+}
+
+
+
