81fadbf9f6ccff358048eb06973d13f03cb8f2bf
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2024-07-22 21:00:06 -0300

    voltei de novo

---
+++ 2.Condicionais/3.Condicional Aninhada.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+float nota;
+
+int main(){
+    printf("Digite sua nota:");
+    scanf("%f",&nota);
+
+    if(nota>=7){
+        printf("APROVADO\n");
+    }else{
+        if(nota>=5){
+            printf("RECUPERAÇÃO\n");
+        }else{
+            printf("APROVADO\n");
+        }
+    }
+    return 0;
+}
