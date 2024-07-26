87ead7398199d79110816bba5cd96e771fc7e091
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2024-07-26 12:54:29 -0300

    void

--- 6.Vetores/3.Entrada e saída de dados com loop.c
+++ 6.Vetores/3.Entrada e saída de dados com loop.c
@@ -4,12 +4,13 @@ int vetor[5];
 
 
 int main(){
+    //Entrada de dados
     for(int i=1;i<=5;i++){
         printf("Digite o %d.o número: ",i);
         scanf("%d",&vetor[i]);
     }
 
-
+    //Saídade de dados
     printf("[");
     for(int i=1;i<=5;i++){
         if(i==5){
