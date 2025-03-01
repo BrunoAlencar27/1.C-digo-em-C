c1a9c3c100652082e5d7581fb5d57f67ec75344d
BrunoAlencar27 <Bruno.alencar.2017@gmail.com>
2025-03-01 20:36:36 -0300

    esse

---
+++ 9. Ponteiros/1. Variável simples.c
@@ -0,0 +1,11 @@
+//Algoritmo:Ponteiro para variável simples
+//Registro:DeVup
+
+#include <stdio.h>
+
+int main(){
+    int ex1;    //Variável simples
+    int *p=&ex1;//Ponteiro para uma variável simples
+
+    return 0;
+}
---
+++ 9. Ponteiros/2. Vetor.c
@@ -0,0 +1,11 @@
+//Algoritmo:Ponteiro para um vetor
+//Registro:DeVup
+
+#include <stdio.h>
+
+int main(){
+    int vetor[10];  //Vetor
+    int *p=vetor;   //Ponteiro para um vetor
+
+    return 0;
+}
